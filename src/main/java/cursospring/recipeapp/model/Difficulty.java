a4d20cb1f782a4cc2bcf6be7a4873ad7faabe85f
federicofrontera <federicofrontera@gmail.com>
2019-09-22 10:52:36 -0300

    Added Difficulty to Recipe as an enum. Closes #7.

---
+++ src/main/java/cursospring/recipeapp/model/Difficulty.java
@@ -0,0 +1,5 @@
+package cursospring.recipeapp.model;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/cursospring/recipeapp/model/Recipe.java
+++ src/main/java/cursospring/recipeapp/model/Recipe.java
@@ -20,10 +20,15 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
     @Lob
     private Byte[] image;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     public Long getId() {
         return id;
@@ -103,5 +108,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }

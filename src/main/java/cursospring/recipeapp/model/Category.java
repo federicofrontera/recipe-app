5629f25fd27ce8228d3fdf1176928225f3d8a86a
federicofrontera <federicofrontera@gmail.com>
2019-09-22 11:12:43 -0300

    Added ManyToMany Recipe-Category Relationship. Closes #9.

--- src/main/java/cursospring/recipeapp/model/Category.java
+++ src/main/java/cursospring/recipeapp/model/Category.java
@@ -1,18 +1,17 @@
 package cursospring.recipeapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 public class Category {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String description;
+
+    @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
     public Long getId() {

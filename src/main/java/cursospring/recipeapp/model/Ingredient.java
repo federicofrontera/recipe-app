9e75fe941b29501b299b8ba851eb95655c85aa1f
federicofrontera <federicofrontera@gmail.com>
2019-09-22 09:51:06 -0300

    Added JPA-relationship OneToOne Ingredient -> UnitOfMeasure. Closes #6.

--- src/main/java/cursospring/recipeapp/model/Ingredient.java
+++ src/main/java/cursospring/recipeapp/model/Ingredient.java
@@ -9,8 +9,12 @@ public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private BigDecimal amount;
+
+    @OneToOne
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -37,6 +41,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 
     public Recipe getRecipe() {

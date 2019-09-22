ef949cb23e64680d930ed0db011e976c73ff78f7
federicofrontera <federicofrontera@gmail.com>
2019-09-22 09:53:58 -0300

    Added missing getters and setters in UnitOfMeasure entity.

--- src/main/java/cursospring/recipeapp/model/UnitOfMeasure.java
+++ src/main/java/cursospring/recipeapp/model/UnitOfMeasure.java
@@ -11,7 +11,21 @@ public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String uom;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
 }

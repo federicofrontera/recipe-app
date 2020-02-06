a6a03286e6cb486f2e1d6ccd81962e13f97e4f9f
federicofrontera <federicofrontera@gmail.com>
2020-02-06 16:37:42 -0300

    added RecipeController for showing recipes by ID. Only shows description so far. #15

--- src/main/java/cursospring/recipeapp/bootstrap/RecipeBootStrap.java
+++ src/main/java/cursospring/recipeapp/bootstrap/RecipeBootStrap.java
@@ -76,7 +76,7 @@ public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEven
         }
 
         //get optionals
-        UnitOfMeasure eachUom = eachUomOptional.get();
+            UnitOfMeasure eachUom = eachUomOptional.get();
         UnitOfMeasure tableSpoonUom = tableSpoonUomOptional.get();
         UnitOfMeasure teapoonUom = tableSpoonUomOptional.get();
         UnitOfMeasure dashUom = dashUomOptional.get();
---
+++ src/test/java/cursospring/recipeapp/controllers/RecipeControllerTest.java
@@ -0,0 +1,46 @@
+package cursospring.recipeapp.controllers;
+
+import cursospring.recipeapp.model.Recipe;
+import cursospring.recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController recipeController;
+
+    @Before
+    public void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
+
+        recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void getRecipe() throws Exception{
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+
+    }
+}

74b75d3e70a4d5acef8602055e336d7cc4d38b97
federicofrontera <federicofrontera@gmail.com>
2020-02-11 16:26:06 -0300

    implemented converters for UoM <> UoMCommand.#17

--- src/main/java/cursospring/recipeapp/converters/UnitOfMeasureCommandToUnitOfMeasure.java
+++ src/main/java/cursospring/recipeapp/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -1,4 +1,22 @@
 package cursospring.recipeapp.converters;
 
-public class UnitOfMeasureCommandToUnitOfMeasure {
+import cursospring.recipeapp.commands.UnitOfMeasureCommand;
+import cursospring.recipeapp.model.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure> {
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand source) {
+        if (source == null) {
+            return null;
+        }
+        final UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(source.getId());
+        uom.setDescription(source.getDescription());
+        return uom;
+    }
 }
--- src/main/java/cursospring/recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
+++ src/main/java/cursospring/recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -1,4 +1,23 @@
 package cursospring.recipeapp.converters;
 
-public class UnitOfMeasureToUnitOfMeasureCommand {
+import cursospring.recipeapp.commands.UnitOfMeasureCommand;
+import cursospring.recipeapp.model.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure source) {
+        if (source == null) {
+            return null;
+        }
+        final UnitOfMeasureCommand uomCommand = new UnitOfMeasureCommand();
+        uomCommand.setId(source.getId());
+        uomCommand.setDescription(source.getDescription());
+        return uomCommand;
+    }
 }

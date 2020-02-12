968eddc0295eb9f8cf4930d6e675857319bf1bb2
federicofrontera <federicofrontera@gmail.com>
2020-02-12 15:38:25 -0300

    implemented category<>categoryCommand converter. #17.

--- src/main/java/cursospring/recipeapp/converters/CategoryCommandToCategory.java
+++ src/main/java/cursospring/recipeapp/converters/CategoryCommandToCategory.java
@@ -1,4 +1,26 @@
 package cursospring.recipeapp.converters;
 
-public class CategoryCommandToCategory {
+import cursospring.recipeapp.commands.CategoryCommand;
+import cursospring.recipeapp.model.Category;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Category category = new Category();
+
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+
+        return category;
+    }
 }
--- src/main/java/cursospring/recipeapp/converters/CategoryToCategoryCommand.java
+++ src/main/java/cursospring/recipeapp/converters/CategoryToCategoryCommand.java
@@ -1,4 +1,26 @@
 package cursospring.recipeapp.converters;
 
-public class CategoryToCategoryCommand {
+import cursospring.recipeapp.commands.CategoryCommand;
+import cursospring.recipeapp.model.Category;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class CategoryToCategoryCommand implements Converter<Category, CategoryCommand> {
+    @Synchronized
+    @Nullable
+    @Override
+    public CategoryCommand convert(Category source) {
+        if (source == null) {
+            return null;
+        }
+
+        final CategoryCommand categoryCommand = new CategoryCommand();
+
+        categoryCommand.setId(source.getId());
+        categoryCommand.setDescription(source.getDescription());
+
+        return categoryCommand;
+
+    }
 }

4d1950f59c72984c95252ae23f48bd837acf4b1e
federicofrontera <federicofrontera@gmail.com>
2020-02-12 16:24:39 -0300

    implemented notes<>notesCommand converters. #17.

--- src/main/java/cursospring/recipeapp/converters/NotesCommandToNotes.java
+++ src/main/java/cursospring/recipeapp/converters/NotesCommandToNotes.java
@@ -1,4 +1,23 @@
 package cursospring.recipeapp.converters;
 
-public class NotesCommandToNotes {
+import cursospring.recipeapp.commands.NotesCommand;
+import cursospring.recipeapp.model.Notes;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class NotesCommandToNotes implements Converter<NotesCommand, Notes> {
+    @Override
+    @Synchronized
+    @Nullable
+    public Notes convert(NotesCommand source) {
+        if(source == null) {
+            return null;
+        }
+
+        final Notes notes = new Notes();
+        notes.setId(source.getId());
+        notes.setRecipeNotes(source.getRecipeNotes());
+        return notes;
+    }
 }
--- src/main/java/cursospring/recipeapp/converters/NotesToNotesCommand.java
+++ src/main/java/cursospring/recipeapp/converters/NotesToNotesCommand.java
@@ -1,4 +1,23 @@
 package cursospring.recipeapp.converters;
 
-public class NotesToNotesCommand {
+import cursospring.recipeapp.commands.NotesCommand;
+import cursospring.recipeapp.model.Notes;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class NotesToNotesCommand implements Converter<Notes, NotesCommand> {
+    @Override
+    @Synchronized
+    @Nullable
+    public NotesCommand convert(Notes source) {
+        if (source == null) {
+            return null;
+        }
+
+        final NotesCommand notesCommand = new NotesCommand();
+        notesCommand.setId(source.getId());
+        notesCommand.setRecipeNotes(source.getRecipeNotes());
+        return notesCommand;
+    }
 }

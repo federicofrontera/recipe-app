156d83b62e6325249d8c9a10f839dd8860ba17ed
federicofrontera <federicofrontera@gmail.com>
2020-02-12 16:12:22 -0300

    implemented ingredient<>ingredientCommand converters. #17.

--- src/main/java/cursospring/recipeapp/converters/IngredientCommandToIngredient.java
+++ src/main/java/cursospring/recipeapp/converters/IngredientCommandToIngredient.java
@@ -1,4 +1,33 @@
 package cursospring.recipeapp.converters;
 
-public class IngredientCommandToIngredient {
+import cursospring.recipeapp.commands.IngredientCommand;
+import cursospring.recipeapp.model.Ingredient;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+
+public class IngredientCommandToIngredient implements Converter<IngredientCommand, Ingredient> {
+    private final UnitOfMeasureCommandToUnitOfMeasure converter;
+
+    public IngredientCommandToIngredient(UnitOfMeasureCommandToUnitOfMeasure converter) {
+        this.converter = converter;
+    }
+
+
+    @Override
+    @Synchronized
+    @Nullable
+    public Ingredient convert(IngredientCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Ingredient ingredient = new Ingredient();
+        ingredient.setId(source.getId());
+        ingredient.setAmount(source.getAmount());
+        ingredient.setDescription(source.getDescription());
+        ingredient.setUom(converter.convert(source.getUnitOfMeasure()));
+        return ingredient;
+    }
 }
--- src/main/java/cursospring/recipeapp/converters/IngredientToIngredientCommand.java
+++ src/main/java/cursospring/recipeapp/converters/IngredientToIngredientCommand.java
@@ -1,4 +1,28 @@
 package cursospring.recipeapp.converters;
 
-public class IngredientToIngredientCommand {
+import cursospring.recipeapp.commands.IngredientCommand;
+import cursospring.recipeapp.model.Ingredient;
+import org.springframework.core.convert.converter.Converter;
+
+public class IngredientToIngredientCommand implements Converter<Ingredient, IngredientCommand> {
+    private final UnitOfMeasureToUnitOfMeasureCommand converter;
+
+    public IngredientToIngredientCommand(UnitOfMeasureToUnitOfMeasureCommand converter) {
+        this.converter = converter;
+    }
+
+    @Override
+    public IngredientCommand convert(Ingredient source) {
+        if (source == null) {
+            return null;
+        }
+
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setId(source.getId());
+        ingredientCommand.setAmount(source.getAmount());
+        ingredientCommand.setDescription(source.getDescription());
+        ingredientCommand.setUnitOfMeasure(converter.convert(source.getUom()));
+        return ingredientCommand;
+    }
+
 }

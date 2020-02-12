e6e239ed38f76dba6c1101da89855efa97fd3ce6
federicofrontera <federicofrontera@gmail.com>
2020-02-12 16:24:16 -0300

    added notes<>notesCommand tests. #17.

---
+++ src/test/java/cursospring/recipeapp/converters/NotesCommandToNotesTest.java
@@ -0,0 +1,43 @@
+package cursospring.recipeapp.converters;
+
+import cursospring.recipeapp.commands.NotesCommand;
+import cursospring.recipeapp.model.Notes;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NotesCommandToNotesTest {
+    public static final Long ID_VALUE = Long.valueOf(1L);
+    public static final String RECIPE_NOTES = "Notes";
+    NotesCommandToNotes converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new NotesCommandToNotes();
+
+    }
+
+    @Test
+    public void testNullParameter() throws Exception {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() throws Exception {
+        assertNotNull(converter.convert(new NotesCommand()));
+    }
+
+    @Test
+    public void convert() throws Exception {
+        NotesCommand notesCommand = new NotesCommand();
+        notesCommand.setId(ID_VALUE);
+        notesCommand.setRecipeNotes(RECIPE_NOTES);
+
+        Notes notes = converter.convert(notesCommand);
+
+        assertNotNull(notes);
+        assertEquals(ID_VALUE, notes.getId());
+        assertEquals(RECIPE_NOTES, notes.getRecipeNotes());
+    }
+}

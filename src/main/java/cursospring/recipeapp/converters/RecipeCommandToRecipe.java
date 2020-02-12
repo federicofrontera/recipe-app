86429971599867080f640b3a9eb0f2ca400206ba
federicofrontera <federicofrontera@gmail.com>
2020-02-12 16:36:34 -0300

    added tests and implementation for recipe<>recipeCommand. #17.

--- src/main/java/cursospring/recipeapp/converters/RecipeCommandToRecipe.java
+++ src/main/java/cursospring/recipeapp/converters/RecipeCommandToRecipe.java
@@ -1,4 +1,54 @@
 package cursospring.recipeapp.converters;
 
-public class RecipeCommandToRecipe {
+import cursospring.recipeapp.commands.RecipeCommand;
+import cursospring.recipeapp.model.Recipe;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
+    private final CategoryCommandToCategory categoryConverter;
+    private final NotesCommandToNotes notesConverter;
+    private final IngredientCommandToIngredient ingredientConverter;
+
+    public RecipeCommandToRecipe(CategoryCommandToCategory categoryCommandToCategory,
+                                 IngredientCommandToIngredient ingredientCommandToIngredient,
+                                 NotesCommandToNotes notesCommandToNotes) {
+        this.categoryConverter = categoryCommandToCategory;
+        this.ingredientConverter = ingredientCommandToIngredient;
+        this.notesConverter = notesCommandToNotes;
+    }
+
+    @Override
+    @Synchronized
+    @Nullable
+    public Recipe convert(RecipeCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Recipe recipe = new Recipe();
+        recipe.setId(source.getId());
+        recipe.setCookTime(source.getCookTime());
+        recipe.setPrepTime(source.getPrepTime());
+        recipe.setDescription(source.getDescription());
+        recipe.setDifficulty(source.getDifficulty());
+        recipe.setDirections(source.getDirections());
+        recipe.setServings(source.getServings());
+        recipe.setSource(source.getSource());
+        recipe.setUrl(source.getUrl());
+        recipe.setNotes(notesConverter.convert(source.getNotes()));
+
+        if (source.getCategories() != null && source.getCategories().size() > 0){
+            source.getCategories()
+                    .forEach( category -> recipe.getCategories().add(categoryConverter.convert(category)));
+        }
+
+        if (source.getIngredients() != null && source.getIngredients().size() > 0){
+            source.getIngredients()
+                    .forEach(ingredient -> recipe.getIngredients().add(ingredientConverter.convert(ingredient)));
+        }
+
+        return recipe;
+    }
 }
--- src/main/java/cursospring/recipeapp/converters/RecipeToRecipeCommand.java
+++ src/main/java/cursospring/recipeapp/converters/RecipeToRecipeCommand.java
@@ -1,4 +1,56 @@
 package cursospring.recipeapp.converters;
 
-public class RecipeToRecipeCommand {
+import cursospring.recipeapp.commands.RecipeCommand;
+import cursospring.recipeapp.model.Category;
+import cursospring.recipeapp.model.Recipe;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class RecipeToRecipeCommand implements Converter<Recipe, RecipeCommand> {
+    private final NotesToNotesCommand notesConverter;
+    private final CategoryToCategoryCommand categoryConverter;
+    private final IngredientToIngredientCommand ingredientConverter;
+
+    public RecipeToRecipeCommand(CategoryToCategoryCommand categoryToCategoryCommand,
+                                 IngredientToIngredientCommand ingredientToIngredientCommand,
+                                 NotesToNotesCommand notesToNotesCommand) {
+        this.categoryConverter = categoryToCategoryCommand;
+        this.ingredientConverter = ingredientToIngredientCommand;
+        this.notesConverter = notesToNotesCommand;
+    }
+
+
+    @Override
+    @Synchronized
+    @Nullable
+    public RecipeCommand convert(Recipe source) {
+        if (source == null) {
+            return null;
+        }
+
+        final RecipeCommand command = new RecipeCommand();
+        command.setId(source.getId());
+        command.setCookTime(source.getCookTime());
+        command.setPrepTime(source.getPrepTime());
+        command.setDescription(source.getDescription());
+        command.setDifficulty(source.getDifficulty());
+        command.setDirections(source.getDirections());
+        command.setServings(source.getServings());
+        command.setSource(source.getSource());
+        command.setUrl(source.getUrl());
+        command.setNotes(notesConverter.convert(source.getNotes()));
+
+        if (source.getCategories() != null && source.getCategories().size() > 0) {
+            source.getCategories()
+                    .forEach((Category category) -> command.getCategories().add(categoryConverter.convert(category)));
+        }
+
+        if (source.getIngredients() != null && source.getIngredients().size() > 0) {
+            source.getIngredients()
+                    .forEach(ingredient -> command.getIngredients().add(ingredientConverter.convert(ingredient)));
+        }
+
+        return command;
+    }
 }

a3d0b4bcbd81262b55c1082af9e5975dc1bd3e89
federicofrontera <federicofrontera@gmail.com>
2020-02-17 15:53:21 -0300

    added form for adding new recipes. Missing implementation for difficulty, ingredients and categories. #18.

--- src/main/java/cursospring/recipeapp/controllers/RecipeController.java
+++ src/main/java/cursospring/recipeapp/controllers/RecipeController.java
@@ -1,11 +1,11 @@
 package cursospring.recipeapp.controllers;
 
+import cursospring.recipeapp.commands.RecipeCommand;
 import cursospring.recipeapp.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -22,4 +22,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/"+ savedCommand.getId();
+    }
+
 }

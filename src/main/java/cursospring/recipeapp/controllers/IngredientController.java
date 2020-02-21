44f2e883072cb2db01721230d6c8f71fa8174d65
federicofrontera <federicofrontera@gmail.com>
2020-02-21 15:41:35 -0300

    updated html to correctly show ingredient list from the recipe form. Closes #21.

--- src/main/java/cursospring/recipeapp/controllers/IngredientController.java
+++ src/main/java/cursospring/recipeapp/controllers/IngredientController.java
@@ -1,6 +1,7 @@
 package cursospring.recipeapp.controllers;
 
 import cursospring.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,19 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IngredientController {
 
 
     private final RecipeService recipeService;
 
+    @Autowired
     public IngredientController(RecipeService recipeService) {
-    this.recipeService = recipeService;
+        this.recipeService = recipeService;
     }
 
     @GetMapping
     @RequestMapping("/recipe/{recipeId}/ingredients")
     public String listIngredients(@PathVariable String recipeId, Model model){
+        log.debug("Getting ingredient list for recipe: "+ recipeId);
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 
         return "recipe/ingredient/list";
